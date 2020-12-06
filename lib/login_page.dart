48027ed2458148226481b66fe7296d3e145de40c
FikryAS <azizfikry15@gmail.com>
2020-12-06 12:13:10 +0700

    add page login page

---
+++ lib/login_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginPages extends StatefulWidget {
+  @override
+  _LoginPagesState createState() => _LoginPagesState();
+}
+
+class _LoginPagesState extends State<LoginPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
