c8fe88d740941b9936346043727d2a8785bac20c
FikryAS <azizfikry15@gmail.com>
2020-12-07 10:38:55 +0700

    update main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_instagram_inisiated/insta_home.dart';
 import 'package:flutter_clone_instagram_inisiated/login_page.dart';
 import 'package:splashscreen/splashscreen.dart';
 
@@ -14,12 +15,25 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(
-        seconds: 4,
-        navigateAfterSeconds: LoginPages(),
-        image: Image.asset('assets/images/insta_logo2.png'),
-        loadingText: Text('Copyright \u00a9 2020 FikryAS'),
-        backgroundColor: Colors.white,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          primaryIconTheme: IconThemeData(color: Colors.black),
+          primaryTextTheme: TextTheme(
+              // ignore: deprecated_member_use
+              title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
+          // ignore: deprecated_member_use
+          textTheme: TextTheme(title: TextStyle(color: Colors.black)),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Center(
+        child: SplashScreen(
+          seconds: 3,
+          navigateAfterSeconds: InstaHome(),
+          image: Image.asset('assets/images/insta_logo.png'),
+          photoSize: 100.0,
+          loadingText: Text('Copyright \u00a9 2020 FikryAS'),
+          loaderColor: Colors.transparent,
+          backgroundColor: Colors.grey[100],
+        ),
       ),
     );
   }
